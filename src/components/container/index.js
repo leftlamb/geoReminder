11ead08c8d1cc219132058c56f6f47f1a4556e46
Martin Kostveit <martin_kostveit@hotmail.com>
2018-11-08 14:46:46 +0100

    generell design

--- src/components/container/index.js
+++ src/components/container/index.js
@@ -54,6 +54,9 @@ class Container extends Component {
                 center = {this.state.center}
               />
             </div>
+            <div className="infoContainer">
+
+            </div>
           </div>
       );
   }
