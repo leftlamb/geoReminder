0d18aa7d77c8d3d17c6afb5b19fecc8ec2902456
Martin Kostveit <martin_kostveit@hotmail.com>
2019-05-03 16:29:50 +0200

    class

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,11 +2,16 @@ import React, { Component } from 'react'
 import './style.css'
 
 export default class Home extends Component {
-  render() {
-    return (
-      <div className="homeContainer">
+    render() {
+        return (
+            <div className="homeContainer">
+                <div className="mapContainer">
 
-      </div>
-    )
-  }
+                </div>
+                <div className="swipeMenu smLow ">
+
+                </div>
+            </div>
+        )
+    }
 }
